6659fa0609741f6196218b7f4a684a2b38fd9129
Oleksandr <collywolly9798@gmail.com>
2025-05-20 11:23:34 +0200

    done

---
+++ src/components/icons/ArrLeftIcon.jsx
@@ -0,0 +1,10 @@
+const ArrLeftIcon = props => (
+  <svg xmlns='http://www.w3.org/2000/svg' width='15' height='24' fill='none' viewBox='0 0 15 24'>
+    <path
+      fill='#104C9A'
+      d='M11.232.852 1.24 10.747a1.763 1.763 0 0 0 0 2.505l9.993 9.896a1.795 1.795 0 0 0 1.95.384c.327-.134.606-.361.803-.653a1.76 1.76 0 0 0 .301-.984V2.105c0-.351-.105-.694-.301-.985a1.8 1.8 0 0 0-.803-.652 1.81 1.81 0 0 0-1.95.384'
+    ></path>
+  </svg>
+);
+
+export default ArrLeftIcon;
--- src/features/catalogue/UkrainianBrands/UkrainianBrands.jsx
+++ src/features/catalogue/UkrainianBrands/UkrainianBrands.jsx
@@ -1,16 +1,17 @@
 import SmallCarousel from '../../../components/ui/small-carousel/SmallCarousel.tsx';
 import UaPromo from '../UaPromo/UaPromo.jsx';
+import LinkIcon from '@/components/icons/LinkIcon.jsx';
 import './UkrainianBrands.scss';
 
 const images = [
-  '/img/ua-brands/brand-ua-1.png',
-  '/img/ua-brands/brand-ua-2.png',
-  '/img/ua-brands/brand-ua-3.png',
-  '/img/ua-brands/brand-ua-4.png',
-  '/img/ua-brands/brand-ua-1.png',
-  '/img/ua-brands/brand-ua-2.png',
-  '/img/ua-brands/brand-ua-3.png',
-  '/img/ua-brands/brand-ua-4.png',
+  '/img/ua-brands/x-store.png',
+  '/img/ua-brands/ricky-hype.png',
+  '/img/ua-brands/black-limit.png',
+  '/img/ua-brands/norba.png',
+  '/img/ua-brands/x-store.png',
+  '/img/ua-brands/ricky-hype.png',
+  '/img/ua-brands/black-limit.png',
+  '/img/ua-brands/norba.png',
 ];
 
 export default function UkrainianBrands() {
@@ -20,15 +21,7 @@ export default function UkrainianBrands() {
         <h2 className='section-logo'>Ukrainian Brands</h2>
         <a className='section-link' href='#'>
           Shop now
-          <svg width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg'>
-            <path
-              d='M13.6683 7.66217L10 7.66216C8.34315 7.66215 7 9.0053 7 10.6622L7 21.9997C7 23.6566 8.34315 24.9997 10 24.9997L21.3376 24.9997C22.9944 24.9997 24.3376 23.6566 24.3376 21.9997V18.3314M14.3351 17.6646L24.3376 7.66215M19.0029 7.66211L24.3375 7.67188L24.3375 12.9967'
-              stroke='#104C9A'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-            />
-          </svg>
+          <LinkIcon />
         </a>
       </div>
       <SmallCarousel images={images} />

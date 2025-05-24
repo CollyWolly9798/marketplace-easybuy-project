04ac7b2d4e1be3e60c069b4aa20be2304e2919a1
Oleksandr <collywolly9798@gmail.com>
2025-05-24 18:34:46 +0200

    add buttons for auth

---
+++ src/components/icons/ArrRAuth.jsx
@@ -0,0 +1,10 @@
+const ArrRAuth = props => (
+  <svg xmlns='http://www.w3.org/2000/svg' width='6' height='10' fill='none' viewBox='0 0 6 10'>
+    <path
+      fill='#252A31'
+      d='M.289 8.407 3.813 5.22a.332.332 0 0 0 0-.496L.328 1.596A.753.753 0 0 1 .271.534.75.75 0 0 1 1.33.476l4.38 3.933c.332.299.333.82.002 1.119L1.293 9.524a.75.75 0 0 1-1.059-.055.753.753 0 0 1 .055-1.062'
+    ></path>
+  </svg>
+);
+
+export default ArrRAuth;
---
+++ src/components/ui/sign-with/SignWith.jsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import ArrRAuth from '@/components/icons/ArrRAuth.jsx';
+import style from './SignWith.module.scss';
+
+const SignWith = ({ providers }) => {
+  return (
+    <div>
+      <p className={style.text}>Or</p>
+      <ul className={style.list}>
+        {providers.map(({ icon: Icon, name, href }, index) => (
+          <li key={index}>
+            <Link href={href}>
+              <button className={style.btn} type='button' disabled>
+                <Icon />
+                <p className={style.company}>{name}</p>
+                <ArrRAuth />
+              </button>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SignWith;

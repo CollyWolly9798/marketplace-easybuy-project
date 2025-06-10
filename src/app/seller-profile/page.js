d26ee2f1ad5058edbbdd85019ef41acbe43fcb9f
NastasiaKarpenko <nastasia.karpenko@gmail.com>
2025-06-10 12:51:21 +0100

    "added  a new page - seller profile"

---
+++ src/app/seller-profile/page.js
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import SellerProfile from "@/components/SellerProfile";
+
+export default function SellerProfilePage() {
+  return (
+    <div>
+      <h1>Профіль продавця</h1>
+      <SellerProfile />
+    </div>
+  );
+}

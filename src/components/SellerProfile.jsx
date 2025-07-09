5fccdcd0d37c24c2172fe1dd25bc466cb615cb64
NastasiaKarpenko <nastasia.karpenko@gmail.com>
2025-07-09 14:51:29 +0100

    test

--- src/components/SellerProfile.jsx
+++ src/components/SellerProfile.jsx
@@ -1,10 +1,13 @@
 const SellerProfile = () => {
   return (
-    <div>
+    <>
+      <h1 className='text-2xl  font-bold  text-gray-800 mb-4 bg-black  rounded-lg shadow-md '>
+        Профіль продавця
+      </h1>
       <h2 className='text-2xl  font-bold  text-gray-800 mb-4 bg-black  rounded-lg shadow-md '>
         Це інформація для продавця.
       </h2>
-    </div>
+    </>
   );
 };
 export default SellerProfile;
